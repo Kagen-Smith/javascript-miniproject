979d3565cef2f4a0a50266bd88fda6f75449034b
Keith <keith_a_w@hotmail.com>
2024-06-20 22:34:43 -0600

    add html boilerplate, css pseudocode

---
+++ script.js
@@ -0,0 +1,16 @@
+// Entering the the R,P,S
+prompt();
+// Confirm if they want to play again or not
+confirm();
+// Entered invald number 
+alert();
+
+// If rock vs scissors, rock wins. If rock vs paper, rock loses.
+function rock (){}
+
+// If paper vs rock, paper wins. If paper vs scissors, paper loses.
+function paper(){}
+
+// If scissors vs paper, scissors wins. If scissors vs rock, scissors loses. 
+function scissors(){}
+
